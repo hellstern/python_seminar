1cbf9f43f0bd21e6b1c960fa2596db89f1ec6b28
Hellstern <tue@f1-support.dk>
2020-06-08 10:45:57 +0200

    Create 13_tkinter_demo.py

---
+++ docs/13_tkinter_demo.py
@@ -0,0 +1,26 @@
+# tkinter demo - Tæl sekunder
+import tkinter as tk
+
+counter = 0
+
+def counter_label(label):
+    counter = 0
+
+    def count():
+        global counter
+        counter += 1
+        label.config(text=str(counter))
+        label.after(1000, count)
+
+    count()
+
+root = tk.Tk()
+root.title("Antal sekunder")
+label = tk.Label(root, fg="dark green")
+label.pack()
+counter_label(label)
+button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+button.pack()
+
+root.mainloop()
+
