6779bc61eac4b2a18b0064f0e5b59db6b59d84b2
Hellstern <tue@f1-support.dk>
2020-06-08 12:09:32 +0200

    Update 07_csv_excel.py

--- docs/07_csv_excel.py
+++ docs/07_csv_excel.py
@@ -1,19 +1,35 @@
-# csv filer
-import requests
+# Import af CSV og Excel til DataFrame
+# Filer placert lokat
 import pandas as pd
 
+# CSV
+seals_team = pd.read_csv("sales_team.csv")
+print(seals_team)
+
+# Excel
+invoices = pd.read_excel("invoices.xlsx")
+print(invoices)
 
 
-url = 'https://raw.githubusercontent.com/FBosler/Medium-Data-Extraction/master/sales_team.csv'
+
+# Hent fra URL - CSV og Excel
+# Filer er placert på GitHub
+import requests
+
+# CSV
+url = "https://hellstern.github.io/python_seminar/sales_team.csv"
 res = requests.get(url, allow_redirects=True)
-with open('sales_team.csv','wb') as file:
+with open('sales_team.csv', 'wb') as file:
     file.write(res.content)
 sales_team = pd.read_csv('sales_team.csv')
 
+print(sales_team)
 
-# Excel filer
-url = 'https://github.com/FBosler/Medium-Data-Extraction/blob/master/invoices.xlsx?raw=true'
+# Excel
+url = "https://hellstern.github.io/python_seminar/invoices.xlsx"
 res = requests.get(url, allow_redirects=True)
-with open('invoices.xlsx','wb') as file:
+with open('invoices.xlsx', 'wb') as file:
     file.write(res.content)
 invoices = pd.read_excel('invoices.xlsx')
+
+print(invoices)
